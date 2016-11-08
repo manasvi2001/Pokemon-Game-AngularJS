abac02207c39fc742e5676af0b2847fbc61cddc3
Manasvi Priyadarshi <manasvi2001@gmail.com>
2016-11-08 20:01:05 +0530

    Added Signup Page

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('PokemonApp', ['ngRoute', 'PokemonApp.login']);
+var app = angular.module('PokemonApp', ['ngRoute', 'PokemonApp.login', 'PokemonApp.signup', 'PokemonApp.game']);
 
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.otherwise({redirectTo: '/login'});
